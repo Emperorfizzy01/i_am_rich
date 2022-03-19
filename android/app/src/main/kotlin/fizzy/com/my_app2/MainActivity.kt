4b34248ba48f149cdfc226bf39a6838b00753747
Emperorfizzy01 <farukgbolahan21@gmail.com>
2022-03-19 10:43:12 +0100

    First commit

---
+++ android/app/src/main/kotlin/fizzy/com/my_app2/MainActivity.kt
@@ -0,0 +1,6 @@
+package fizzy.com.my_app2
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ android/app/src/main/kotlin/fizzy/com/my_app3/MainActivity.kt
@@ -0,0 +1,6 @@
+package fizzy.com.my_app3
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
